a9b543b6b22eda1b9fb276fd2f70d47334ce1d2e
julpereira <julia.p@estudante.ifro.edu.br>
2024-09-26 11:42:48 -0400

    commit

--- apiNOME/index.js
+++ apiNOME/index.js
@@ -3,27 +3,27 @@ const colecaoNOME = require('./dados/dados');
 
 const app = express();
 
-app.get('/ufs/:iduf', (req, res) => {
-    const idNOME =  parseInt(req.params.iduf)
-    let mensgemErro = '';
-    let uf;
+app.get('/nms/:idnome', (req, res) => {
+    const idNOME =  parseInt(req.params.idnome)
+    let mensagemErro = '';
+    let nome;
 
-    if (!(isNaN(idUF))) {
-        uf = colecaoUF.colecaoUf.find(u => u.id === idUF);
-        if (!NOME) {
+    if (!(isNaN(idNOME))) {
+        nome = colecaoNOME.colecaoNOME.find(u => u.id === idNOME);
+        if (!nome) {
             mensagemErro = 'UF não encontrada';
         }
         }else{
-            mensgemErro = 'Requisição inválida';
+            mensagemErro = 'Requisição inválida';
         }
-    if(uf){
-        res.json(uf);
+    if(nome){
+        res.json(nome);
     
     } else{
         res.status(404).json({"erro": mensagemErro});
     }
 });
 
-app.listen(8080, () =>{
+app.listen(8080, () => {
     console.log('Servidor iniciado na porta 8080');
 });
