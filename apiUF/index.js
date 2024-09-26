b0554d731bc7c0b7e624061b35120969a62b8749
julpereira <julia.p@estudante.ifro.edu.br>
2024-09-26 11:18:58 -0400

    commit

--- apiUF/index.js
+++ apiUF/index.js
@@ -5,8 +5,23 @@ const app = express();
 
 app.get('/ufs/:iduf', (req, res) => {
     const idUF = parseInt(req.params.iduf)
-    const uf = colecaoUF.colecaoUf.find(u => u.id === idUF);
-    res.json(uf);
+    let mensgemErro = '';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUF.colecaoUf.find(u => u.id === idUF);
+        if (!uf) {
+            mensagemErro = 'UF não encontrada';
+        }
+        }else{
+            mensgemErro = 'Requisição inválida';
+        }
+    if(uf){
+        res.json(uf);
+    
+    } else{
+        res.status(404).json({"erro": mensagemErro});
+    }
 });
 
 app.listen(8080, () =>{
