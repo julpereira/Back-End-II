87cd148011fc9b652a21f9ff9c6c0affd880317c
julpereira <julia.p@estudante.ifro.edu.br>
2024-09-26 10:46:16 -0400

    commit

--- apiUF/index.js
+++ apiUF/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const colecaoUF = require('./dados/dados');
 
-const app = express()
+const app = express();
 
-app.get('/ufs', (req, res) => {
-    res.json(colecaoUF.colecaoUf);
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf)
+    const uf = colecaoUF.colecaoUf.find(u => u.id === idUF);
+    res.json(uf);
 });
 
 app.listen(8080, () =>{
     console.log('Servidor iniciado na porta 8080');
-})
+});
