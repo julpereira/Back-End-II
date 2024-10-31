b1d988e4eeb24aa83eb0b477b9d168586ae25ffc
julpereira <julia.p@estudante.ifro.edu.br>
2024-10-31 10:37:27 -0400

    commit

--- apiUF/index.js
+++ apiUF/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
-const colecaoUF = require('./dados/dados');
+import express from 'express';
+import colecaoUf from './dados/dados.js';
 
 const app = express();
+
+const buscarUfsPorNome = (nomeUf) => {
+    return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
+};
+
+app.get('/ufs', (req, res) => {
+    const nomeUf = req.query.busca;
+    const resultado = nomeUf ? buscarUfsPorNome(nomeUf) : colecaoUf;
+    if (resultado.length > 0) {
+        res.json(resultado);
+    } else{
+        res.status(404).send({"erro": "Nenhuma UF encontrada"});
+    }
+});
 
 app.get('/ufs/:iduf', (req, res) => {
     const idUF = parseInt(req.params.iduf)
@@ -9,17 +23,17 @@ app.get('/ufs/:iduf', (req, res) => {
     let uf;
 
     if (!(isNaN(idUF))) {
-        uf = colecaoUF.colecaoUf.find(u => u.id === idUF);
+        uf = colecaoUf.colecaoUf.find(u => u.id === idUF);
+
         if (!uf) {
             mensagemErro = 'UF não encontrada';
         }
-        }else{
+        }else {
             mensgemErro = 'Requisição inválida';
         }
     if(uf){
         res.json(uf);
-    
-    } else{
+    } else {
         res.status(404).json({"erro": mensagemErro});
     }
 });
