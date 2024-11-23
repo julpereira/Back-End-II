a89e6ead0f307865f22905c031f189bbc4103f97
julpereira <julia.p@estudante.ifro.edu.br>
2024-11-23 11:14:04 -0300

    commit

--- api_IPCA/servicos/servico.js
+++ api_IPCA/servicos/servico.js
@@ -42,24 +42,23 @@ export const calcularReajuste = (valor, dataInicialMes, dataInicialAno, dataFina
 
 export const validacaoErro = (valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno) => {
   const anoLimiteFinal = historicoInflacao[historicoInflacao.length - 1].ano;
-  const anoLimiteInicial = historicoInflacao[0].ano
+  const anoLimiteInicial = historicoInflacao[0].ano;
   const mesLimiteFinal = historicoInflacao[historicoInflacao.length - 1].mes;
-  if (
-    isNaN(valor) ||
-    isNaN(dataInicialMes) ||
-    isNaN(dataInicialAno) ||
-    isNaN(dataFinalMes) ||
-    isNaN(dataFinalAno) ||
-    dataInicialMes < 1 || dataInicialMes > 12 ||
-    dataInicialAno < anoLimiteInicial || dataInicialAno > anoLimiteFinal ||
-    dataFinalMes < 1 || dataFinalMes > 12 ||
-    dataFinalAno < anoLimiteInicial || dataFinalAno > anoLimiteFinal ||
-    (dataFinalAno === anoLimiteFinal && dataFinalMes > mesLimiteFinal) ||
-    dataFinalAno < dataInicialAno ||
-    (dataFinalAno == dataInicialAno && dataFinalMes < dataInicialMes)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-};
+
+  if (isNaN(valor)) return true;
+  if (isNaN(dataInicialMes)) return true;
+  if (isNaN(dataInicialAno)) return true;
+  if (isNaN(dataFinalMes)) return true;
+  if (isNaN(dataFinalAno)) return true;
+
+  if (dataInicialMes < 1 || dataInicialMes > 12) return true;
+  if (dataInicialAno < anoLimiteInicial || dataInicialAno > anoLimiteFinal) return true;
+  if (dataFinalMes < 1 || dataFinalMes > 12) return true;
+  if (dataFinalAno < anoLimiteInicial || dataFinalAno > anoLimiteFinal) return true;
+
+  if (dataFinalAno === anoLimiteFinal && dataFinalMes > mesLimiteFinal) return true;
+  if (dataFinalAno < dataInicialAno) return true;
+  if (dataFinalAno === dataInicialAno && dataFinalMes < dataInicialMes) return true;
+
+  return false;
+}
