e859298ae13a4937b1fecca99ee03d5a029e4fff
julpereira <julia.p@estudante.ifro.edu.br>
2025-01-30 11:39:39 -0400

    commit

--- api_LIBERTADORES/index.js
+++ api_LIBERTADORES/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
 // import pool from './servico/conexao.js';
+import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
 
 const app = express();
+
+app.get('/campeonatos', async (req, res) => {
+    const campeonatos = await retornaCampeonatos();
+    res.json(campeonatos);
+})
+
+app.get('/campeonatos:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    const campeonato = await retornaCampeonatosID(id);
+    res.json(campeonato);
+})
 
 app.listen(9000, async () => {
 const data = new Date();
--- api_LIBERTADORES/servico/retornaCampeonatos_servico.js
+++ api_LIBERTADORES/servico/retornaCampeonatos_servico.js
@@ -2,7 +2,7 @@ import pool from './conexao.js';
 
 export async function retornaCampeonatos() {
     const conexao = await pool.getConnection();
-    const campeonatos_tb =await conexao.query ('SELECT, id, campeao, vice, ano FROM campeonatos');
+    const campeonatos_tb =await conexao.query ('SELECT id, campeao, vice, ano FROM campeonatos');
     const campeonatos = campeonatos_tb[0];
     conexao.release();
     return campeonatos;
