13255c166230d000e6f2f3a3eb561eef826401ad
julpereira <julia.p@estudante.ifro.edu.br>
2025-02-13 11:32:39 -0400

    commit

--- api_LIBERTADORES/index.js
+++ api_LIBERTADORES/index.js
@@ -1,19 +1,58 @@
 import express from 'express';
-import cors from "cors";
+import cors from 'cors';
 // import pool from './servico/conexao.js';
-import { retornaCampeonatos, retornaCampeonatosID ,retornaCampeonatosAno, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatos, retornaCampeonatosID, retornaCampeonatosAno, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
 import { cadastraCampeonato } from './servico/cadastroCampeonato_servico.js';
+import { ataulizaCampeonato, atualizaCampeonatoParcial } from './servico/atualizaCampeonato_servico.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json()); //Suporte para JSON no corpo da requisição
 
-app.post('/campeonatos', async(req, res) =>{
+app.patch('/campeonatos/:id', async (req, res) => {
+    const { id } = req.params;
+    const { campeao, vice, ano } = req.body;
+
+    const CamposAtualizar = {};
+    if (campeao) CamposAtualizar.campeao = campeao;
+    if (vice) CamposAtualizar.vice = vice;
+    if (ano) CamposAtualizar.ano = ano;
+
+    if (Object.keys(CamposAtualizar).length === 0){
+        res.status(400).send("Nenhum campo válido foi enviado para atualização.");
+    } else {
+        const resultado = await atualizaCampeonatoParcial(id, CamposAtualizar);
+
+        if (resultado.affectedRows > 0) {
+            res.status(202).send("Registro atualizado com sucesso!");
+        } else {
+            res.status(404).send("Registro não encontrado!");
+        }
+    }
+})
+
+app.put('/campeonatos/:id', async (req, res) => {
+    const { id } = req.params;
+    const { campeao, vice, ano } = req.body;
+
+    if (campeao == undefined || vice == undefined || ano == undefined) {
+        res.status(400).send('Nem todos os campos foram informados.')
+    } else {
+        const resultado = await ataulizaCampeonato(id, campeao, vice, ano);
+        if (resultado.affectedRows > 0) {
+            res.status(202).send('Registro atualizado com sucesso!');
+        } else {
+            res.status(400).send('Registro não encontrado!');
+        }
+    }
+})
+
+app.post('/campeonatos', async (req, res) => {
     const campeao = req.body.campeao;
     const vice = req.body.vice;
     const ano = req.body.ano;
     await cadastraCampeonato(campeao, vice, ano);
-    res.status(204).send({"Mensagem": "Cadastro efetivado com sucesso!"});
+    res.status(204).send({ "Mensagem": "Cadastro efetivado com sucesso!" });
 })
 
 app.get('/campeonatos', async (req, res) => {
@@ -25,14 +64,15 @@ app.get('/campeonatos', async (req, res) => {
         campeonatos = await retornaCampeonatos();
     } else if (typeof ano !== 'undefined') {
         campeonatos = await retornaCampeonatosAno(ano);
-    
-    }else if (typeof time !== 'undefined') {
+
+    } else if (typeof time !== 'undefined') {
         campeonatos = await retornaCampeonatosTime(time);
     }
     if (campeonatos.length > 0) {
         res.json(campeonatos);
-    }   else { 
-        res.status(404).json({ Mensagem: "Nenhum campeonato encontrado"})};
+    } else {
+        res.status(404).json({ Mensagem: "Nenhum campeonato encontrado." })
+    };
 });
 
 app.get('/campeonatos/:id', async (req, res) => {
@@ -40,18 +80,18 @@ app.get('/campeonatos/:id', async (req, res) => {
     const campeonato = await retornaCampeonatosID(id);
     if (campeonato.length > 0) {
         res.json(campeonato);
-    } else{
-        res.status(404).json({ Mensagem: "Nenhum campeonato encontrado"})
+    } else {
+        res.status(404).json({ Mensagem: "Nenhum campeonato encontrado." })
     }
 });
 
 app.listen(9000, async () => {
     const data = new Date();
-    console.log("Servidor node iniciado em:" +data);
+    console.log("Servidor node iniciado em:" + data);
 
-// const conexao = await pool.getConnection();
-// console.log(conexao.threadId);
-// conexao.release();
+    // const conexao = await pool.getConnection();
+    // console.log(conexao.threadId);
+    // conexao.release();
 })
 
 // biblioteca cors
--- api_LIBERTADORES/servico/atualizaCampeonato_servico.js
+++ api_LIBERTADORES/servico/atualizaCampeonato_servico.js
@@ -6,5 +6,20 @@ export async function ataulizaCampeonato(id, campeao, vice, ano) {
     const [resposta] = await conexao.execute(query, [campeao, vice, ano, id]);
     console.log(resposta);
     conexao.release();
-    return resposta();
+    return resposta;
+}
+
+export async function atualizaCampeonatoParcial(id, campos){
+    const conexao = await pool.getConnection();
+
+    const colunas = Object.keys(campos).map(campo => `${campo} = ?`.join(","));
+    const valores = Object.values(campos);
+
+    const query = `UPDATE campeonatos SET ${colunas} WHERE id = ?`;
+    valores.push(id);
+
+    const [resposta] = await conexao.execute(query, valores);
+    console.log(resposta);
+    conexao.release();
+    return resposta;
 }
